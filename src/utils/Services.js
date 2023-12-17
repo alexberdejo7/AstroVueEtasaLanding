1536ea641fa933ff6f4f057227eb0298d2ef06e3
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-12-16 22:30:29 -0600

    todobiencards

---
+++ src/utils/Services.js
@@ -0,0 +1,71 @@
+
+
+const services = [
+    {
+      titulo: 'Estructuras metálicas en general',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion:
+        'Construimos una amplia variedad de estructuras metálicas, con resultados duraderos y funcionales.',
+    },
+    {
+      titulo: 'Servicios de Corte y Rolado de Placa',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion:
+        'Soluciones personalizadas de corte, doblez y rolado de placas de acero. Adaptamos a las necesidades específicas de tu proyecto',
+    },
+    {
+      titulo: 'Estructuras Tridimensionales',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Desarrollo de estructuras de gran envergadura con diseños tridimensionales innovadores.',
+    },
+    {
+      titulo: 'Cubiertas autosoportantes',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Este sistema de coberturas le permite aprovechar al máximo el espacio al no requerir de ningún apoyo intermedio; además de una mayor velocidad de fabricación e instalación.   ',
+    },
+    {
+      titulo: 'Bodegas Comerciales:',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Creamos bodegas eficientes para el almacenamiento seguro de productos y mercancías.',
+    },
+    {
+      titulo: 'Edificios Metálicos:',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Fabricación integral de edificios con perfiles estructurales innovadores y tecnología avanzada para obras únicas.',
+    },
+    {
+      titulo: 'Pailería Versátil',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Soluciones metálicas completas; corte, mecanizado y conformado de placas y perfiles para construcciones adaptables',
+    },
+    {
+      titulo: 'Gasolineras',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Especialistas en la fabricación e instalación de faldones, techumbres y otras partes de estaciones de servicio de combustible.',
+    },
+    {
+      titulo: 'Herrería en general',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Servicios completos de fabricación y diseño de elementos metálicos, desde puertas, escaleras hasta anuncios espectaculares  ',
+    },
+    {
+      titulo: 'Tanques de todo tipo',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Fabricación y montaje de tanques de almacenamiento con presiones considerables y seguridad, proporcionando durabilidad y resistencia ',
+    },
+    {
+      titulo: 'Tubería Especializada',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Ejecución de proyectos de tubería en acero al carbón e inoxidable, asegurando calidad y seguridad en cada detalle  ',
+    },
+    {
+      titulo: 'Naves Industriales',
+      imagen: '../../public/ImagenesEtasa/Card1.jpg',
+      descripcion: 'Diseñamos y montamos naves industriales funcionales y seguras que están perfectamente adaptadas al flujo de producción y almacenamiento.',
+    },
+   
+
+  ]
+  
+  
+  export default services
