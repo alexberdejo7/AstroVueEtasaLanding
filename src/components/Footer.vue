6b7365f48634665187c54e6b5904ef47f9edf73c
alexandro.berdejo <alexandro.berdejo@gmail.com>
2024-01-30 19:16:13 -0600

    GalleryRefactorandCardsFixAddBento

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
 
 <template>
 <footer  class="bg-gray-950 text-white py-12 px-4 sm:px-6 lg:px-8">
-  <div  class="grid gap-8 grid-cols-1 md:grid-cols-3">
+  <div  class="grid gap-8 grid-cols-1 p-3 md:grid-cols-3">
     <div class="space-y-4 text-xs text-white">
       <h4 class="text-lg text-etasa font-semibold">Contáctanos</h4>
       <div class="space-y-2">
---
+++ src/utils/GalleryData.js
@@ -0,0 +1,82 @@
+
+
+const GalleryData = { 
+gallery: [
+
+    {
+      "titulo": "Pista Hípica",
+      "imagenes": [
+        "/EtasaImg/FOTO19.webp",
+        "/EtasaImg/FOTO23.jpg",
+        "/EtasaImg/FOTO24.webp",
+        "/EtasaImg/FOTO210.webp",
+       
+      ]
+    },
+    {
+      "titulo": "Outlet Querétaro",
+      "imagenes": [
+        "/EtasaImg/FOTO55.jpg",
+        "/EtasaImg/FOTO56.jpg",
+        "/EtasaImg/FOTO33.webp",
+        "/EtasaImg/FOTO97.jpg"
+      ]
+    },
+    {
+      "titulo": "Residencia Moderna de Acero",
+      "imagenes": [
+        "/EtasaImg/FOTO112.jpg",
+        "/EtasaImg/FOTO113.webp",
+        "/EtasaImg/FOTO110.jpg",
+        "/EtasaImg/FOTO300.webp"
+      ]
+    },
+    {
+      "titulo": "Casa de Estructura de Acero",
+      "imagenes": [
+        "/EtasaImg/FOTO90.webp",
+        "/EtasaImg/FOTO34.jpg",
+        "/EtasaImg/FOTO99.jpg",
+        "/EtasaImg/FOTO13.jpg"
+      ]
+    },
+    {
+      "titulo": "Departamento Ahuehuetes",
+      "imagenes": [
+        "/EtasaImg/FOTO92.webp",
+        "/EtasaImg/FOTO57.webp",
+        "/EtasaImg/FOTO9.jpg",
+        "/EtasaImg/ahuehuetes.webp"
+      ]
+    },
+    {
+      "titulo": "Depósito Dental",
+      "imagenes": [
+        "/EtasaImg/FOTO93.jpg",
+        "/EtasaImg/FOTO205.webp",
+        "/EtasaImg/FOTO203.webp",
+        "/EtasaImg/FOTO6.webp"
+      ]
+    },
+    {
+      "titulo": "Centro de Reproducción Equina",
+      "imagenes": [
+        "/EtasaImg/FOTO50.jpg",
+        "/EtasaImg/FOTO45.webp",
+        "/EtasaImg/FOTO21.webp",
+        "/EtasaImg/FOTO26.webp"
+      ]
+    },
+    {
+      "titulo": "Otros",
+      "imagenes": [
+        "/EtasaImg/FOTO151.jpg",
+        "/EtasaImg/FOTO156.jpg",
+        "/EtasaImg/FOTO153.jpg",
+        "/EtasaImg/FOTO155.JPG"
+      ]
+    },
+  ]
+}
+
+export default GalleryData
